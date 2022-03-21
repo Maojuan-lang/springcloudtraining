68068b08d1a5772f6d71820547816b0331c9b55c
Maojuan <75521125+Maojuan-lang@users.noreply.github.com>
2022-03-21 19:43:57 +0800

    Add files via upload

---
+++ cloud-config-center-3344/src/main/java/com/thenema/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.thenema.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author xiaoxiang
+ * @data 2022/3/12 14:54
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
